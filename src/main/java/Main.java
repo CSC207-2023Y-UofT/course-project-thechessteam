93256dedd7b5ee52f35030840a35f195cbcd3ffe
DT <dariotaoasus@gmail.com>
2023-07-18 11:06:34 -0400

    Created separate main class and removed sample Hello World files

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    // Main method for testing
+    public static void main(String[] args) {
+        String[][] chessBoard = {
+                {"r", "n", "b", "q", "k", "b", "n", "r"},
+                {"p", "p", "p", "p", "p", "p", "p", "p"},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", " ", " ", " ", " "},
+                {"P", "P", "P", "P", "P", "P", "P", "P"},
+                {"R", "N", "B", "Q", "K", "B", "N", "R"}
+        };
+
+        // Create a ChessBoard object with the initial board setup
+        ChessBoard cb = new ChessBoard(chessBoard);
+        // Print the ChessBoard object
+        System.out.println(cb);
+    }
+}
