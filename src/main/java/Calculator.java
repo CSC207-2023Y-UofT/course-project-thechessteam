89ad71008f78915e70eee033ad7658a64e2f50e5
Dar T <53280779+taodario@users.noreply.github.com>
2023-07-20 16:12:31 -0400

    Update Calculator.java
    Added new method parameters, passing in LocationBitboard

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,14 +5,14 @@ public interface Calculator {
     // Calculates all valid moves a player can make for the piece at from.
     // from is a bitboard with 1 at the location of the piece and 0s for the rest.
     // Returns a bitboard with 1s at valid moves and 0s for the rest.
-    long valid_moves(long from, int side);
+    long valid_moves(long from, int side, LocationBitboard board);
 
     // Calculates the attack coverage of the piece type inheriting Calculator interface.
     // Note: It is attack coverage so only diagonal moves of pawns are attacks.
     // Used to check for check.
     // For example, for Pawn class, it could use the bit "or" operation to combine bitboards in pawn_attacks
     // Returns a bitboard with 1s at "can attack" locations and 0s for the rest.
-    long attack_coverage(int side);
+    long attack_coverage(int side, LocationBitboard board);
 
     // TODO Create Pawn, Rook, Knight, Bishop, Queen, and King Class and have them implement Calculator.
     // TODO Implement the methods accordingly.
