e0594f5b1fc0a967c849c3f96a9fb06698d6372b
DT <dariotaoasus@gmail.com>
2023-07-21 01:31:49 -0400

    Implemented the Bishop class valid_moves method, after making some final changes.

--- src/main/java/Bishop.java
+++ src/main/java/Bishop.java
@@ -1,4 +1,5 @@
 public class Bishop implements Calculator {
+    private long sameColoredPieces;
 
     /**
      * @param from current location, long
@@ -8,7 +9,6 @@ public class Bishop implements Calculator {
      */
     @Override
     public long valid_moves(long from, int side, LocationBitboard board) {
-//        long binaryS=1L<<s;
         // due to occupied being duplicate code, may want to make it reusable.
         // occupied provides occupied places on the board. (recall, OR | operator)
         long occupied = board.whiteBishop[0] | board.whiteKing[0] | board.whiteRook[0] | board.whiteKnight[0] | board.whitePawn[0] | board.whiteQueen[0] |
@@ -18,7 +18,21 @@ public class Bishop implements Calculator {
         int s = Long.numberOfTrailingZeros(from);
         long possibilitiesDiagonal = ((occupied&FileAndRank.DiagonalMasks8[(s / 8) + (s % 8)]) - (2 * from)) ^ Long.reverse(Long.reverse(occupied&FileAndRank.DiagonalMasks8[(s / 8) + (s % 8)]) - (2 * Long.reverse(from)));
         long possibilitiesAntiDiagonal = ((occupied&FileAndRank.AntiDiagonalMasks8[(s / 8) + 7 - (s % 8)]) - (2 * from)) ^ Long.reverse(Long.reverse(occupied&FileAndRank.AntiDiagonalMasks8[(s / 8) + 7 - (s % 8)]) - (2 * Long.reverse(from)));
-        return (possibilitiesDiagonal&FileAndRank.DiagonalMasks8[(s / 8) + (s % 8)]) | (possibilitiesAntiDiagonal&FileAndRank.AntiDiagonalMasks8[(s / 8) + 7 - (s % 8)]);
+
+        // depending on whether you are black or white side (white == 0), generates the pieces that are the same color as the piece in question (i.e., can't step on your own pieces).
+        this.sameColoredPieces = (side == 0) ?
+                board.whitePawn[0] | board.whiteRook[0] | board.whiteKnight[0]
+                        | board.whiteBishop[0] | board.whiteQueen[0] | board.whiteKing[0] :
+                board.blackPawn[0] | board.blackRook[0] | board.blackKnight[0]
+                        | board.blackBishop[0] | board.blackQueen[0] | board.blackKing[0];
+
+        // return long, of the valid moves this specific Rook may take
+        return calculateFinalPosition((possibilitiesDiagonal&FileAndRank.DiagonalMasks8[(s / 8) + (s % 8)])) | calculateFinalPosition(possibilitiesAntiDiagonal&FileAndRank.AntiDiagonalMasks8[(s / 8) + 7 - (s % 8)]);
+    }
+
+    // helper method, to calculate the final position (makes sure piece in question can't step on their own colored pieces)
+    public long calculateFinalPosition(long candidate) {
+        return candidate & ~this.sameColoredPieces;
     }
 
     /**
