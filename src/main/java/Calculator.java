f986f42d4fe5e3383db9a6f3a43969ef90539250
DT <dariotaoasus@gmail.com>
2023-07-22 16:18:10 -0400

    Added bishop tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -18,56 +18,56 @@ public interface Calculator {
     // TODO Implement the methods accordingly.
     // TODO Use PreCalculatedAttacks accordingly.
 
-    public class King implements Calculator {
-
-        // Create this King class for each king and update LocationBitboard when the king moves.
-
-        private final LocationBitboard board;
-
-        public King(LocationBitboard board) {
-            this.board = board;
-        }
-
-        @Override
-        public long valid_moves(long from, int side) {
-            int square = Long.numberOfTrailingZeros(from);
-
-            // Calculate possible king attacks from the current square
-            long attacks = PreCalculatedAttacks.king_attacks[square];
-
-            // You can't move into a square that's attacked by an opponent's piece
-            long opponentAttacks = (side == 0) ? attack_coverage(1) : attack_coverage(0);
-
-            // You can't move into a square occupied by one of your own pieces
-            long ownPieces = (side == 0) ?
-                    (board.whitePawn[0] | board.whiteKnight[0] | board.whiteBishop[0] |
-                            board.whiteRook[0] | board.whiteQueen[0] | board.whiteKing[0]) :
-                    (board.blackPawn[0] | board.blackKnight[0] | board.blackBishop[0] |
-                            board.blackRook[0] | board.blackQueen[0] | board.blackKing[0]);
-
-            // Remove the squares that the king can't move into
-            return attacks & ~opponentAttacks & ~ownPieces;
-        }
-
-        @Override
-        public long attack_coverage(int side) {
-            long kings = (side == 0) ? board.whiteKing[0] : board.blackKing[0];
-            long attackBoard = 0L;
-
-            // Loop through each king
-            while (kings != 0) {
-                int kingLocation = Long.numberOfTrailingZeros(kings);
-
-                // Add the attacks for this king to the attackBoard
-                attackBoard |= PreCalculatedAttacks.king_attacks[kingLocation];
-
-                // Remove this king from the kings bitboard
-                kings &= kings - 1;
-            }
-
-            return attackBoard;
-        }
-    }
+//    public class King implements Calculator {
+//
+//        // Create this King class for each king and update LocationBitboard when the king moves.
+//
+//        private final LocationBitboard board;
+//
+//        public King(LocationBitboard board) {
+//            this.board = board;
+//        }
+//
+//        @Override
+//        public long valid_moves(long from, int side) {
+//            int square = Long.numberOfTrailingZeros(from);
+//
+//            // Calculate possible king attacks from the current square
+//            long attacks = PreCalculatedAttacks.king_attacks[square];
+//
+//            // You can't move into a square that's attacked by an opponent's piece
+//            long opponentAttacks = (side == 0) ? attack_coverage(1) : attack_coverage(0);
+//
+//            // You can't move into a square occupied by one of your own pieces
+//            long ownPieces = (side == 0) ?
+//                    (board.whitePawn[0] | board.whiteKnight[0] | board.whiteBishop[0] |
+//                            board.whiteRook[0] | board.whiteQueen[0] | board.whiteKing[0]) :
+//                    (board.blackPawn[0] | board.blackKnight[0] | board.blackBishop[0] |
+//                            board.blackRook[0] | board.blackQueen[0] | board.blackKing[0]);
+//
+//            // Remove the squares that the king can't move into
+//            return attacks & ~opponentAttacks & ~ownPieces;
+//        }
+//
+//        @Override
+//        public long attack_coverage(int side) {
+//            long kings = (side == 0) ? board.whiteKing[0] : board.blackKing[0];
+//            long attackBoard = 0L;
+//
+//            // Loop through each king
+//            while (kings != 0) {
+//                int kingLocation = Long.numberOfTrailingZeros(kings);
+//
+//                // Add the attacks for this king to the attackBoard
+//                attackBoard |= PreCalculatedAttacks.king_attacks[kingLocation];
+//
+//                // Remove this king from the kings bitboard
+//                kings &= kings - 1;
+//            }
+//
+//            return attackBoard;
+//        }
+//    }
 
 
 
