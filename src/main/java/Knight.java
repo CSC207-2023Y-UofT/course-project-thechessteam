ae7b56fa71632d0ed8446e8c3214140b70dd63b6
onahharr <harrisononah@hotmail.com>
2023-07-23 08:53:31 -0400

    - Added comments for the knight class

--- src/main/java/Knight.java
+++ src/main/java/Knight.java
@@ -1,19 +1,24 @@
 public class Knight implements Calculator {
-
+    // Knight Class intended to hold the calculations for knight piece movements
     private long sameColoredPieces;
 
     public Knight() {}
 
     @Override
+    // method that calculates valid moves for knight
     public long valid_moves(long from, int side, LocationBitboard board) {
+
+        // Amount of empty bits in front of piece
         int s = Long.numberOfTrailingZeros(from);
 
+        // Gets the pieces of the same team
         this.sameColoredPieces = (side == 0) ?
                 board.whitePawn[0] | board.whiteRook[0] | board.whiteKnight[0]
                         | board.whiteBishop[0] | board.whiteQueen[0] | board.whiteKing[0] :
                 board.blackPawn[0] | board.blackRook[0] | board.blackKnight[0]
                         | board.blackBishop[0] | board.blackQueen[0] | board.blackKing[0];
 
+        // calculates the position of knight
         return calculateFinalPosition(PreCalculatedAttacks.knight_attacks[s]);
     }
 
