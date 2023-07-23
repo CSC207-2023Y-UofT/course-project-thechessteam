02e5ec497b223d29fb1d64db74d297d6f3cc6d88
onahharr <harrisononah@hotmail.com>
2023-07-23 08:43:04 -0400

    - Finished Knight Class

--- src/main/java/Knight.java
+++ src/main/java/Knight.java
@@ -6,14 +6,7 @@ public class Knight implements Calculator {
 
     @Override
     public long valid_moves(long from, int side, LocationBitboard board) {
-
-        long occupied = board.whiteBishop[0] | board.whiteKing[0] | board.whiteRook[0] | board.whiteKnight[0] | board.whitePawn[0] | board.whiteQueen[0] |
-                board.blackBishop[0] | board.blackKing[0] | board.blackRook[0] | board.blackKnight[0] | board.blackPawn[0] | board.blackQueen[0] ;
-
         int s = Long.numberOfTrailingZeros(from);
-        // TODO calculations for possibleHorizontalMoves & possible vertical moves
-        // long possibleHorizontalMoves =
-        // long possibleVertical =
 
         this.sameColoredPieces = (side == 0) ?
                 board.whitePawn[0] | board.whiteRook[0] | board.whiteKnight[0]
@@ -21,10 +14,7 @@ public class Knight implements Calculator {
                 board.blackPawn[0] | board.blackRook[0] | board.blackKnight[0]
                         | board.blackBishop[0] | board.blackQueen[0] | board.blackKing[0];
 
-        // TODO return calculateFinalPosition instead of placeholder 1
-        // return calculateFinalPosition();
-
-        return 1;
+        return calculateFinalPosition(PreCalculatedAttacks.knight_attacks[s]);
     }
 
     public long calculateFinalPosition(long candidate) {
@@ -33,11 +23,21 @@ public class Knight implements Calculator {
 
     @Override
     public long attack_coverage(int side, LocationBitboard board) {
-        long attacked = 0L;  // bits where rook is attacking
+        long attacked = 0L;  // bits where knight is attacking
         long knights_location;  // knights location based on team
 
+        if (side == 0) {  // white side
+            knights_location = board.whiteKnight[0];
+        } else {  // black side
+            knights_location = board.blackKnight[0];
+        }
 
-        // TODO calculations for attack coverage for the knight
+        for (int i = 0; i < 64; i++) {
+            // Shifting knight location
+            if ((int) ((knights_location >>> i) & 1) == 1) {
+                attacked |= valid_moves(1L << i, side, board);
+            }
+        }
 
         return attacked;
     }
