6518811686c9ad8d0c272e8b6fba23cb727ec31d
123Chan <39209661+123Chan@users.noreply.github.com>
2023-07-23 20:23:42 -0400

    not relevant file

--- src/main/java/MovePiece.java
+++
@@ -1,8 +0,0 @@
-public class MovePiece {
-    // Use case class for moving pieces, including castling.
-    public static boolean move_piece(ChessGame currentGame, long start, long end) {
-        // TODO Check if moving from start to end is a valid move. Implement ValidMove use case class.
-        // if (not valid) {return false} else {
-        return currentGame.move_piece(start, end); //}
-    }
-}
