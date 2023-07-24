0bffcc7545bdee31132560f5ef0eb04e8cfb48bc
123Chan <cwoh0605@gmail.com>
2023-07-24 08:03:56 -0400

    Implemented MovePiece and ActualValidMove use case classes. Added entities that the use cases require.

--- src/main/java/CheckCalculator.java
+++ src/main/java/CheckCalculator.java
@@ -1,16 +1,16 @@
 public class CheckCalculator {
 
-    public boolean is_in_check(int side, LocationBitboard board) {
+    public static boolean is_in_check(int side, LocationBitboard board) {
         long opponentAttacks = 0L;
         int opponentSide = 1 - side;
 
         // Get all valid moves for each type of opponent piece and combine them to get the attack coverage
         // Replace the dummy functions with actual implementation
-        opponentAttacks |= Pawn.attack_coverage(opponentSide, board);
-        opponentAttacks |= Rook.attack_coverage(opponentSide, board);
-        opponentAttacks |= Knight.attack_coverage(opponentSide, board);
-        opponentAttacks |= Bishop.attack_coverage(opponentSide, board);
-        opponentAttacks |= Queen.attack_coverage(opponentSide, board);
+        opponentAttacks |= Calculators.pawnCalculator.attack_coverage(opponentSide, board);
+        opponentAttacks |= Calculators.rookCalculator.attack_coverage(opponentSide, board);
+        opponentAttacks |= Calculators.knightCalculator.attack_coverage(opponentSide, board);
+        opponentAttacks |= Calculators.bishopCalculator.attack_coverage(opponentSide, board);
+        opponentAttacks |= Calculators.queenCalculator.attack_coverage(opponentSide, board);
 
         // No need to include the opponent's king in the attack coverage
 
