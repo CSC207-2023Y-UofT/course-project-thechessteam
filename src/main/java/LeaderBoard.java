208d554f16aa7beb3755208abc7778f9774472f9
Anika Nawar <nawarani47@gmail.com>
2023-08-01 22:10:12 -0400

    Added comments

--- src/main/java/LeaderBoard.java
+++ src/main/java/LeaderBoard.java
@@ -35,11 +35,15 @@ public class LeaderBoard{
     private static ArrayList<Player> readData() {
         ArrayList<Player> leaderboard = new ArrayList<>(); // start with an empty array to populate
         try {
+            // read file
             Scanner player_read = new Scanner(new File("players.txt"));
             while (player_read.hasNext()) {
+                // split line by comma into name and wins
                 String[] current_player = player_read.nextLine().split(",");
+                // create new player with name and wins and add it to leaderboard
                 leaderboard.add(new Player(current_player[0], Integer.parseInt(current_player[1])));
             }
+            // close file
             player_read.close();
         } catch (IOException e) {
             System.out.println("No Players in leaderboard yet");
@@ -50,8 +54,10 @@ public class LeaderBoard{
     // write file
     private static void writeData(ArrayList<Player> leaderboard) {
         try {
+            // open players.txt to write in it
             PrintWriter player_output = new PrintWriter("players.txt");
             for (Player p: leaderboard) {
+                // write each player in as you loop through it
                 player_output.println(p.getName() + "," + p.getWins());
             }
             player_output.close(); //close file
@@ -62,8 +68,11 @@ public class LeaderBoard{
 
     //write file v2
     public static void addPlayer(String player_name) {
+        // read file into leaderboard var
         ArrayList<Player> leaderboard = readData();
+        // process name
         String input = player_name.trim().toLowerCase();
+        // check whether the player is already in leaderboard, add a win if it is
         boolean existing_player = false;
         for (Player p : leaderboard) {
             if (Objects.equals(p.getName(), input)) {
@@ -71,6 +80,7 @@ public class LeaderBoard{
                 p.addWins();
             }
         }
+        // if new player, create new player and add to the leaderboard
         if (!(existing_player)) {
             Player new_player = new Player(input, 1);
             leaderboard.add(new_player);
@@ -85,10 +95,12 @@ public class LeaderBoard{
 
     // print leaderboard
     public static void printData() {
+        // read file
         ArrayList<Player> leaderboard = readData();
         if (leaderboard.size() > 0) {
             System.out.println("Leaderboard");
             System.out.println("Player Name -> Wins");
+            // print each player and wins
             for (Player p: leaderboard) {
                 System.out.println(p.getName() + " -> " + p.getWins());
             }
