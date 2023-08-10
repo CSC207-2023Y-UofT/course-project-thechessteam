1c1b0a00efc32f138ea0f40514967c3c68a1fe66
DT <dariotaoasus@gmail.com>
2023-08-10 15:41:07 -0400

    Commenting out the rook

--- src/test/java/RookTest.java
+++ src/test/java/RookTest.java
@@ -1,158 +1,158 @@
-import Entities.Locations.LocationBitboard;
-import Entities.Pieces.Rook;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-class RookTest {
-
-    Rook rookCalculator = new Rook();
-    LocationBitboard board = new LocationBitboard();
-
-    // Valid Move Tests
-
-    @Test
-    void rookMovesAtCorner() {
-        // The array representation of rookLocation
-        int[][] rookLocation = {
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0}
-        };
-
-        // Remove all pieces from board.
-        TestHelper.remove_all_pieces(board);
-        board.blackRook[0] = TestHelper.bitboard_representation(rookLocation);
-        board.updateLocationVariables();
-
-        int[][] expectedValidMove = {
-                {0, 1, 1, 1, 1, 1, 1, 1},
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0}
-        };
-
-        long validMove = rookCalculator.valid_moves(TestHelper.bitboard_representation(rookLocation), false, board);
-        // Check if valid move is as expected
-        assertEquals(TestHelper.bitboard_representation(expectedValidMove), validMove);
-    }
-
-
-    @Test
-    void rookMovesAtOrigin() {
-        // The array representation of rookLocation
-        int[][] rookLocation = {
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 1}
-        };
-
-        // We don't remove all pieces here because we want the pieces that can block its original movement.
-        board.whiteRook[0] = TestHelper.bitboard_representation(rookLocation);
-        board.updateLocationVariables();
-
-        int[][] expectedValidMove = {
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0}
-        };
-
-        long validMove = rookCalculator.valid_moves(TestHelper.bitboard_representation(rookLocation), true, board);
-        // Check if valid move is as expected
-        assertEquals(TestHelper.bitboard_representation(expectedValidMove), validMove);
-    }
-
-    @Test
-    void rookFreeMoveAtOrigin() {
-        // The array representation of rookLocation
-        int[][] rookLocation = {
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 1}
-        };
-
-        // Remove all pieces from board.
-        TestHelper.remove_all_pieces(board);
-        board.whiteRook[0] = TestHelper.bitboard_representation(rookLocation);
-        board.updateLocationVariables();
-
-        int[][] expectedValidMove = {
-                {0, 0, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 0, 0, 0, 0, 1},
-                {0, 0, 0, 0, 0, 0, 0, 1},
-                {1, 1, 1, 1, 1, 1, 1, 0}
-        };
-
-        long validMove = rookCalculator.valid_moves(TestHelper.bitboard_representation(rookLocation), true, board);
-        // Check if valid move is as expected
-        assertEquals(TestHelper.bitboard_representation(expectedValidMove), validMove);
-    }
-
-
-    // ----------------------------------------------------------------------------------------------------------
-    // Attack Coverage Tests
-
-    @Test
-    void rookAttackCoverage() {
-        // The array representation of rookLocation
-        int[][] rookLocation = {
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0}
-        };
-
-        // Remove all pieces from board.
-        TestHelper.remove_all_pieces(board);
-        board.whiteRook[0] = TestHelper.bitboard_representation(rookLocation);
-        board.updateLocationVariables();
-
-        int[][] expectedAttackCoverage = {
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {1, 1, 1, 0, 1, 1, 1, 1},
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0}
-        };
-
-        long attackCoverage = rookCalculator.attack_coverage(true, board);
-        // Check if valid move is as expected
-        assertEquals(TestHelper.bitboard_representation(expectedAttackCoverage), attackCoverage);
-    }
-}
+//import Entities.Locations.LocationBitboard;
+//import Entities.Pieces.Rook;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//
+//class RookTest {
+//
+//    Rook rookCalculator = new Rook();
+//    LocationBitboard board = new LocationBitboard();
+//
+//    // Valid Move Tests
+//
+//    @Test
+//    void rookMovesAtCorner() {
+//        // The array representation of rookLocation
+//        int[][] rookLocation = {
+//                {1, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0}
+//        };
+//
+//        // Remove all pieces from board.
+//        TestHelper.remove_all_pieces(board);
+//        board.blackRook[0] = TestHelper.bitboard_representation(rookLocation);
+//        board.updateLocationVariables();
+//
+//        int[][] expectedValidMove = {
+//                {0, 1, 1, 1, 1, 1, 1, 1},
+//                {1, 0, 0, 0, 0, 0, 0, 0},
+//                {1, 0, 0, 0, 0, 0, 0, 0},
+//                {1, 0, 0, 0, 0, 0, 0, 0},
+//                {1, 0, 0, 0, 0, 0, 0, 0},
+//                {1, 0, 0, 0, 0, 0, 0, 0},
+//                {1, 0, 0, 0, 0, 0, 0, 0},
+//                {1, 0, 0, 0, 0, 0, 0, 0}
+//        };
+//
+//        long validMove = rookCalculator.valid_moves(TestHelper.bitboard_representation(rookLocation), false, board);
+//        // Check if valid move is as expected
+//        assertEquals(TestHelper.bitboard_representation(expectedValidMove), validMove);
+//    }
+//
+//
+//    @Test
+//    void rookMovesAtOrigin() {
+//        // The array representation of rookLocation
+//        int[][] rookLocation = {
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 1}
+//        };
+//
+//        // We don't remove all pieces here because we want the pieces that can block its original movement.
+//        board.whiteRook[0] = TestHelper.bitboard_representation(rookLocation);
+//        board.updateLocationVariables();
+//
+//        int[][] expectedValidMove = {
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0}
+//        };
+//
+//        long validMove = rookCalculator.valid_moves(TestHelper.bitboard_representation(rookLocation), true, board);
+//        // Check if valid move is as expected
+//        assertEquals(TestHelper.bitboard_representation(expectedValidMove), validMove);
+//    }
+//
+//    @Test
+//    void rookFreeMoveAtOrigin() {
+//        // The array representation of rookLocation
+//        int[][] rookLocation = {
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 1}
+//        };
+//
+//        // Remove all pieces from board.
+//        TestHelper.remove_all_pieces(board);
+//        board.whiteRook[0] = TestHelper.bitboard_representation(rookLocation);
+//        board.updateLocationVariables();
+//
+//        int[][] expectedValidMove = {
+//                {0, 0, 0, 0, 0, 0, 0, 1},
+//                {0, 0, 0, 0, 0, 0, 0, 1},
+//                {0, 0, 0, 0, 0, 0, 0, 1},
+//                {0, 0, 0, 0, 0, 0, 0, 1},
+//                {0, 0, 0, 0, 0, 0, 0, 1},
+//                {0, 0, 0, 0, 0, 0, 0, 1},
+//                {0, 0, 0, 0, 0, 0, 0, 1},
+//                {1, 1, 1, 1, 1, 1, 1, 0}
+//        };
+//
+//        long validMove = rookCalculator.valid_moves(TestHelper.bitboard_representation(rookLocation), true, board);
+//        // Check if valid move is as expected
+//        assertEquals(TestHelper.bitboard_representation(expectedValidMove), validMove);
+//    }
+//
+//
+//    // ----------------------------------------------------------------------------------------------------------
+//    // Attack Coverage Tests
+//
+//    @Test
+//    void rookAttackCoverage() {
+//        // The array representation of rookLocation
+//        int[][] rookLocation = {
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 1, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0}
+//        };
+//
+//        // Remove all pieces from board.
+//        TestHelper.remove_all_pieces(board);
+//        board.whiteRook[0] = TestHelper.bitboard_representation(rookLocation);
+//        board.updateLocationVariables();
+//
+//        int[][] expectedAttackCoverage = {
+//                {0, 0, 0, 1, 0, 0, 0, 0},
+//                {0, 0, 0, 1, 0, 0, 0, 0},
+//                {0, 0, 0, 1, 0, 0, 0, 0},
+//                {0, 0, 0, 1, 0, 0, 0, 0},
+//                {1, 1, 1, 0, 1, 1, 1, 1},
+//                {0, 0, 0, 1, 0, 0, 0, 0},
+//                {0, 0, 0, 1, 0, 0, 0, 0},
+//                {0, 0, 0, 1, 0, 0, 0, 0}
+//        };
+//
+//        long attackCoverage = rookCalculator.attack_coverage(true, board);
+//        // Check if valid move is as expected
+//        assertEquals(TestHelper.bitboard_representation(expectedAttackCoverage), attackCoverage);
+//    }
+//}
