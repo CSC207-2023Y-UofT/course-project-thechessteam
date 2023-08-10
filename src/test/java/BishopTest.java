907220167f1499e4bc19025e13217bb3b4e12749
DT <dariotaoasus@gmail.com>
2023-08-10 15:21:43 -0400

    Added bishop test

---
+++ src/test/java/BishopTest.java
@@ -0,0 +1,312 @@
+import Entities.Locations.LocationBitboard;
+import Entities.Pieces.Bishop;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class BishopTest {
+
+    Bishop bishopCalculator = new Bishop();
+    LocationBitboard board = new LocationBitboard();
+
+    // Valid Move Tests
+
+    @Test
+    void bishopMovesAtCorner() {
+        // The array representation of bishopLocation
+        int[][] bishopLocation = {
+                {1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        // Remove all pieces from board.
+        TestHelper.remove_all_pieces(board);
+        board.blackBishop[0] = TestHelper.bitboard_representation(bishopLocation);
+        board.updateLocationVariables();
+
+        int[][] expectedValidMove = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1}
+        };
+
+        long validMove = bishopCalculator.valid_moves(TestHelper.bitboard_representation(bishopLocation), false, board);
+        // Check if valid move is as expected
+        assertEquals(TestHelper.bitboard_representation(expectedValidMove), validMove);
+    }
+
+
+    @Test
+    void bishopMovesAtOrigin() {
+        // The array representation of bishopLocation
+        int[][] bishopLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0}
+        };
+
+        // We don't remove all pieces here because we want the pieces that can block its original movement.
+        board.whiteBishop[0] = TestHelper.bitboard_representation(bishopLocation);
+        board.updateLocationVariables();
+
+        int[][] expectedValidMove = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        long validMove = bishopCalculator.valid_moves(TestHelper.bitboard_representation(bishopLocation), true, board);
+        // Check if valid move is as expected
+        assertEquals(TestHelper.bitboard_representation(expectedValidMove), validMove);
+    }
+
+    @Test
+    void bishopFreeMoveAtOrigin() {
+        // The array representation of bishopLocation
+        int[][] bishopLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 0}
+        };
+
+        // Remove all pieces from board.
+        TestHelper.remove_all_pieces(board);
+        board.whiteBishop[0] = TestHelper.bitboard_representation(bishopLocation);
+        board.updateLocationVariables();
+
+        int[][] expectedValidMove = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        long validMove = bishopCalculator.valid_moves(TestHelper.bitboard_representation(bishopLocation), true, board);
+        // Check if valid move is as expected
+        assertEquals(TestHelper.bitboard_representation(expectedValidMove), validMove);
+    }
+
+
+    // ----------------------------------------------------------------------------------------------------------
+    // Attack Coverage Tests
+
+    @Test
+    void bishopAttackCoverage() {
+        // The array representation of bishopLocation
+        int[][] bishopLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        // Remove all pieces from board.
+        TestHelper.remove_all_pieces(board);
+        board.whiteBishop[0] = TestHelper.bitboard_representation(bishopLocation);
+        board.updateLocationVariables();
+
+        int[][] expectedAttackCoverage = {
+                {0, 0, 0, 0, 0, 0, 0, 1},
+                {1, 0, 0, 0, 0, 0, 1, 0},
+                {0, 1, 0, 0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 1, 0, 0},
+                {1, 0, 0, 0, 0, 0, 1, 0}
+        };
+
+        long attackCoverage = bishopCalculator.attack_coverage(true, board);
+        // Check if valid move is as expected
+        assertEquals(TestHelper.bitboard_representation(expectedAttackCoverage), attackCoverage);
+    }
+
+    @Test
+    void bishopAttackCoverageAllyBlocking() {
+        // The array representation of bishopLocation
+        int[][] bishopLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        int[][] pawnLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        // Remove all pieces from board.
+        TestHelper.remove_all_pieces(board);
+        board.whiteBishop[0] = TestHelper.bitboard_representation(bishopLocation);
+        board.whitePawn[0] = TestHelper.bitboard_representation(pawnLocation);
+        board.updateLocationVariables();
+
+        int[][] expectedAttackCoverage = {
+                {1, 0, 0, 0, 0, 0, 1, 0},
+                {0, 1, 0, 0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        long attackCoverage = bishopCalculator.attack_coverage(true, board);
+        // Check if valid move is as expected
+        assertEquals(TestHelper.bitboard_representation(expectedAttackCoverage), attackCoverage);
+    }
+
+    @Test
+    void bishopAttackCoverageWithEnemy() {
+        // The array representation of bishopLocation
+        int[][] bishopLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        int[][] pawnLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        // Remove all pieces from board.
+        TestHelper.remove_all_pieces(board);
+        board.whiteBishop[0] = TestHelper.bitboard_representation(bishopLocation);
+        board.blackPawn[0] = TestHelper.bitboard_representation(pawnLocation);
+        board.updateLocationVariables();
+
+        int[][] expectedAttackCoverage = {
+                {1, 0, 0, 0, 0, 0, 1, 0},
+                {0, 1, 0, 0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        long attackCoverage = bishopCalculator.attack_coverage(true, board);
+        // Check if valid move is as expected
+        assertEquals(TestHelper.bitboard_representation(expectedAttackCoverage), attackCoverage);
+    }
+
+    @Test
+    void bishopAttackCoverageEnemyAndAlly() {
+        // The array representation of bishopLocation
+        int[][] blackBishopLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        int[][] whitePawnLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        int[][] blackPawnLocation = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        // Remove all pieces from board.
+        TestHelper.remove_all_pieces(board);
+        board.blackBishop[0] = TestHelper.bitboard_representation(blackBishopLocation);
+        board.whitePawn[0] = TestHelper.bitboard_representation(whitePawnLocation);
+        board.blackPawn[0] = TestHelper.bitboard_representation(blackPawnLocation);
+        board.updateLocationVariables();
+
+        int[][] expectedAttackCoverage = {
+                {0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        long attackCoverage = bishopCalculator.attack_coverage(false, board);
+        // Check if valid move is as expected
+        assertEquals(TestHelper.bitboard_representation(expectedAttackCoverage), attackCoverage);
+    }
+
+}
