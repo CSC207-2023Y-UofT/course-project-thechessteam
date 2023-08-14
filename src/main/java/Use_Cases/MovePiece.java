d19e5653067966c5dfe0e9797824c212f7cd34af
abubakarmohidin <abubakar.mohidin@mail.utoronto.ca>
2023-08-13 20:04:37 -0400

    A rewrite of the CheckmateCalculator and StalemateCalculator Classes in order to work with the MVC project layout.
    Adds the GameStateCalculator.java is order to avoid duplicate code to appeal to the Clean Architecture principles

--- src/main/java/Use_Cases/MovePiece.java
+++ src/main/java/Use_Cases/MovePiece.java
@@ -5,27 +5,50 @@ import Entities.ChessGame;
 import Entities.Locations.LocationBitboard;
 import Presenter_Interface.PresenterInterface;
 
+/**
+ * A use case class that handles the operations related to moving chess pieces.
+ */
 public class MovePiece {
-    // Use case class for moving pieces, including castling.
-    // Returns true if there's a piece at start and end is a valid move of that piece.
+
+    // Interface to communicate with the presenter layer
     private PresenterInterface presenter;
+
+    // Current game instance
     private final ChessGame currentGame;
+
+    // Utility to check for valid moves
     private final ActualValidCalculator actualValidCalc;
+
+    /**
+     * Constructor for the MovePiece use case.
+     *
+     * @param currentGame       The current game instance.
+     * @param actualValidCalc   The utility to check for valid moves.
+     */
     public MovePiece(ChessGame currentGame, ActualValidCalculator actualValidCalc) {
         this.currentGame = currentGame;
         this.actualValidCalc = actualValidCalc;
     }
+
+    /**
+     * Sets the presenter instance for this use case.
+     *
+     * @param presenter   The presenter instance.
+     */
     public void set_presenter(PresenterInterface presenter) {
         this.presenter = presenter;
     }
-    public void move_piece(long start, long end) {
 
+    /**
+     * Handles the movement of a chess piece from a start position to an end position.
+     *
+     * @param start   The starting position of the piece.
+     * @param end     The ending position of the piece.
+     */
+    public void move_piece(long start, long end) {
         boolean turn = currentGame.getTurn();
         LocationBitboard currentBoard = currentGame.getCurrentBoard();
         boolean moveSuccessful = move_piece_helper(currentBoard, start, end, turn);
-
-        System.out.println(moveSuccessful);
-        System.out.println();
 
         if (moveSuccessful) {
             for (int i = 0; i < 6; i++) {
@@ -39,47 +62,58 @@ public class MovePiece {
         }
     }
 
+    /**
+     * Fetches the current board state.
+     *
+     * @return The current board state.
+     */
     public LocationBitboard getCurrentBoard() {
         return this.currentGame.getCurrentBoard();
     }
 
+    /**
+     * Fetches the current player turn.
+     *
+     * @return True if it's white's turn, false otherwise.
+     */
     public boolean getCurrentTurn() {
         return this.currentGame.getTurn();
     }
 
+    /**
+     * Handles game-over scenarios.
+     */
     public void handleGameOver() {
         presenter.notifyGameOver();
         // You can add any other game-over related logic here, if needed.
     }
 
+    /**
+     * Handles stalemate scenarios.
+     */
     public void handleStalemate() {
         presenter.notifyGameOver();
         // You can add any other stalemate-related logic here, if needed.
     }
 
-
-
-    public  boolean move_piece_helper(LocationBitboard currentBoard, long start, long end, boolean turn) {
-        boolean canMove = false;
-        if (turn) { // White's turn
-            if ((start & currentBoard.getWhiteLocations()) != 0L) {
-                canMove = true;
-            }
-        } else {
-            if ((start & currentBoard.getBlackLocations()) != 0L) {
-                canMove = true;
-            }
-        }
+    /**
+     * Assists in the movement of a chess piece, ensuring it adheres to the game rules.
+     *
+     * @param currentBoard   The current board state.
+     * @param start          The starting position of the piece.
+     * @param end            The ending position of the piece.
+     * @param turn           True if it's white's turn, false otherwise.
+     * @return               True if the move is successful, false otherwise.
+     */
+    private boolean move_piece_helper(LocationBitboard currentBoard, long start, long end, boolean turn) {
+        boolean canMove = (turn && (start & currentBoard.getWhiteLocations()) != 0L)
+                || (!turn && (start & currentBoard.getBlackLocations()) != 0L);
         if (canMove) {
-            // Check if end is a valid move
+            // Check if the desired end position is a valid move
             if ((end & actualValidCalc.actual_valid_moves(start, turn, currentBoard)) != 0) {
                 return currentBoard.move_piece(start, end, turn);
-            } else { // If end is not a valid move, do not move piece.
-                return false;
             }
         }
-        else { // We do not move piece.
-            return false;
-        }
+        return false;
     }
 }
